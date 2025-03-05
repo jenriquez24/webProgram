26bd449bd6b05a228e82b2db15ab292f589679c4
Buby Enriquez <benriquez@farmaciasperuanas.pe>
2025-03-05 18:33:38 -0500

    herencia

---
+++ JavaScript/Proyecto_JS/clases_02/clase_02.js
@@ -0,0 +1,22 @@
+class Papel {
+    constructor(alto,ancho){
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+}
+
+let Papel_A = class{
+    constructor(alto,ancho){
+        this.alto = alto;
+        this.ancho = ancho;
+
+    }
+}
+
+let Papel_B = class Papelx{
+    constructor(alto,ancho){
+        this.alto = alto;
+        this.ancho = ancho;
+
+    }
+}
