111f91c7f83e25df2d6cf874f9a74eb3c3982a39
Junior Enriquez <jerif.42@gmail.com>
2025-03-05 21:26:58 -0500

    regulaizar cambios

--- JavaScript/Proyecto_JS/temporizador/set_time_out.js
+++ JavaScript/Proyecto_JS/temporizador/set_time_out.js
@@ -1,9 +1,10 @@
+let  elementoSegundos = parseInt(document.getElementById("tiempoelegido").value); 
 
 function comenzarTiempo(){
-    setTimeout(tiempocumplido,5000);
+    setTimeout(tiempocumplido,elementoSegundos);
 }
 
 function tiempocumplido(){
-    alert("Se terminó el tiempo");
+    alert(`Se terminó el tiempo de ${elementoSegundos} segundos`);
 }
 
