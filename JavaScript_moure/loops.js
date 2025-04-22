802ba5d273c664d27baca999ea98d2e03174cfbe
Junior Enriquez <jerif.42@gmail.com>
2025-04-21 22:10:21 -0500

    loops

---
+++ JavaScript_moure/loops.js
@@ -0,0 +1,33 @@
+// loops 
+
+for(let i = 0; i < 5 ;i ++){
+
+    console.log(`Hola esto es JavaScript ${i + 1}`);
+}
+
+const numbers = [1,"b",3,"d",5]
+
+for( let i=0; i < numbers.length; i++ ){
+    console.log(`Este es el elemento, ${numbers[i]}`);
+}
+
+// // while
+
+let i = 0;
+while(i < 5){
+    console.log(`Hola con while ${i}`)
+    i++;
+}
+
+// do while
+
+ i = 6;
+do{
+    console.log(`hola con do while ${i}`)
+    i++;
+}while( i < 5)
+
+
+// for of
+
+
