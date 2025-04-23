7904c771af92d19349742e5310ed71d2cc4b8a8c
Junior Enriquez <jerif.42@gmail.com>
2025-04-22 21:03:47 -0500

    funt

---
+++ JavaScript_moure/funciones.js
@@ -0,0 +1,17 @@
+//funciones
+
+function myFunction(){
+    for(let i =0; i< 5; i++){
+        console.log("Hola esta es una function");
+    }
+}
+
+myFunction();
+
+// con parametros
+
+function myFunctionWhitParams(name){
+    console.log(`Hola, ${name}`);
+}
+
+myFunctionWhitParams("Junior");
